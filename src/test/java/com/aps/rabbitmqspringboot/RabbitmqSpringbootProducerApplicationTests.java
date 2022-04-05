b9dc4bb1dfd60e294b4c59f1bb2af207eed08f0f
Anderson Souza <andersonpds14@gmail.com>
2022-04-05 19:15:18 -0300

    Add rabbit configuration;

---
+++ src/test/java/com/aps/rabbitmqspringboot/RabbitmqSpringbootProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aps.rabbitmqspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqSpringbootProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
